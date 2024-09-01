62cc7743651fa272ac7445dbcc8b7c04d4ca4dbe
Mbuvi1998 <mbuvidan67@gmail.com>
2024-09-01 22:24:12 +0300

    modified reactRecipeSharingApp

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,6 +1,6 @@
 // src/components/DeleteRecipeButton.jsx
 import React from 'react';
-import useRecipeStore from '../store/recipeStore';
+import useRecipeStore from './recipeStore';
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
