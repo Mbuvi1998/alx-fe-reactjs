77ec784084bf2d4213a497c8103407b23d6d7c3d
Mbuvi1998 <mbuvidan67@gmail.com>
2024-09-22 15:43:36 +0300

    added changes

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -6,7 +6,7 @@
 //   },
 //   plugins: [],
 // }
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
   darkMode: false, // or 'media' or 'class'
